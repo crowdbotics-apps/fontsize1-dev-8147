1ab35758be30d17485cd70f7eeb6d30230188d2b
Crowdbotics <team@crowdbotics.com>
2020-07-24 16:29:49 +0000

    Initial commit.

---
+++ backend/fontsize1_dev_8147/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for fontsize1_dev_8147 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fontsize1_dev_8147.settings')
+
+application = get_wsgi_application()
